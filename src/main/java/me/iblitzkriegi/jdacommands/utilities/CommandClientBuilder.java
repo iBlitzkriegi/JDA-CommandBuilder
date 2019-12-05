79c52bcee5debfda63ea53b25753b467f555f2ff
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-05 18:41:15 -0500

    Make CommandClientBuilder actually login via token

--- src/main/java/me/iblitzkriegi/jdacommands/utilities/CommandClientBuilder.java
+++ src/main/java/me/iblitzkriegi/jdacommands/utilities/CommandClientBuilder.java
@@ -3,12 +3,14 @@ package me.iblitzkriegi.jdacommands.utilities;
 import com.google.common.reflect.ClassPath;
 import me.iblitzkriegi.jdacommands.annotations.CommandAnnotation;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CommandClientBuilder {
 
+    public static HashMap<String, Command> commandHashMap = new HashMap<>();
     public boolean isReady;
     public HashMap<String, Command> commands = new HashMap<>();
     private String token = null;
@@ -25,13 +27,22 @@ public class CommandClientBuilder {
     }
 
     public JDA build(Class mainClass) {
+        JDABuilder jdaBuilder = new JDABuilder();
+        JDA jda;
+        jdaBuilder.setToken(this.token);
+        try {
+            jda = jdaBuilder.build().awaitReady();
+        } catch (Exception x) {
+            System.out.printf("An error occured while attempting to login to the bot. Maybe the token is wrong?");
+            return null;
+        }
         for (Class clazz : getAllClasses(mainClass)) {
             if (!clazz.isAnnotationPresent(CommandAnnotation.CommandInfo.class)) {
                 continue;
             }
-            System.out.println("Found in: " + clazz.getSimpleName());
+            CommandAnnotation.CommandInfo commandInfo = (CommandAnnotation.CommandInfo) clazz.getAnnotation(CommandAnnotation.CommandInfo.class);
         }
-        return null;
+        return jda;
     }
 
     public ArrayList<Class> getAllClasses(Class input) {
