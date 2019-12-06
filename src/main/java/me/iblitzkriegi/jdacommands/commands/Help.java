9f142d0e36f93e443ce6bc47707e9f390f060d26
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-06 00:08:28 -0500

    Setup command information and foundation

--- src/main/java/me/iblitzkriegi/jdacommands/commands/Help.java
+++ src/main/java/me/iblitzkriegi/jdacommands/commands/Help.java
@@ -1,11 +1,21 @@
 package me.iblitzkriegi.jdacommands.commands;
 
+import me.iblitzkriegi.jdacommands.annotations.CommandInfo;
 import me.iblitzkriegi.jdacommands.utilities.Command;
 import me.iblitzkriegi.jdacommands.utilities.wrappers.CommandEvent;
 
+@CommandInfo(
+        name = "Help",
+        desc = "The help menu.",
+        usage = "help [<command>]"
+)
 public class Help extends Command {
     @Override
     public void execute(CommandEvent event, String[] args) {
-        
+        if (args.length == 0) {
+            //Show default menu with all registered commands
+            return;
+        }
+        //Take args[0] as a specified command and check CommandClientbuilder#commandHashMap for the command.
     }
 }
