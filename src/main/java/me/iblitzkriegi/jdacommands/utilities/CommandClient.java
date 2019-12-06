d413a256d47561a0ec319f9ead652edef6eb90b7
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-05 19:12:23 -0500

    Implement listener to listen for commands

--- src/main/java/me/iblitzkriegi/jdacommands/utilities/CommandClient.java
+++ src/main/java/me/iblitzkriegi/jdacommands/utilities/CommandClient.java
@@ -1,6 +1,18 @@
 package me.iblitzkriegi.jdacommands.utilities;
 
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class CommandClient extends ListenerAdapter {
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String[] arguments = event.getMessage().getContentDisplay().split(" ");
+        for (BuiltCommand builtCommand : CommandClientBuilder.commandHashMap.values()) {
+            if (arguments[0].contains(CommandClientBuilder.commandStart + builtCommand.getName().toLowerCase())) {
+                builtCommand.getCommandClass().execute(event, arguments);
+            }
+        }
+    }
+
 }
