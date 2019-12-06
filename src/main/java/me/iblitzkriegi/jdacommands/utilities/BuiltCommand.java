39baa8a49b85e1301ead4479cd00275081e5d172
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-05 19:13:04 -0500

    Add instantiated class parameter to allow for super class calling the execute method

--- src/main/java/me/iblitzkriegi/jdacommands/utilities/BuiltCommand.java
+++ src/main/java/me/iblitzkriegi/jdacommands/utilities/BuiltCommand.java
@@ -5,15 +5,21 @@ public class BuiltCommand {
     private String name;
     private String description;
     private String usage;
+    private Command commandClass;
 
-    public BuiltCommand(String name, String description, String usage) {
+    public BuiltCommand(String name, String description, String usage, Command commandClass) {
         this.name = name;
         this.description = description;
         this.usage = usage;
+        this.commandClass = commandClass;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Command getCommandClass() {
+        return this.commandClass;
     }
 
     public void setName(String name) {
@@ -35,4 +41,5 @@ public class BuiltCommand {
     public void setUsage(String usage) {
         this.usage = usage;
     }
+
 }
--- src/main/java/me/iblitzkriegi/jdacommands/utilities/Command.java
+++ src/main/java/me/iblitzkriegi/jdacommands/utilities/Command.java
@@ -1,7 +1,7 @@
 package me.iblitzkriegi.jdacommands.utilities;
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public abstract class Command {
-    public abstract void execute(MessageReceivedEvent e, String[] args);
+    public abstract void execute(GuildMessageReceivedEvent e, String[] args);
 }
