499a9eccfbde25196634777b76b187384e1dc780
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-06 00:06:12 -0500

    Begin working on default help menu command

---
+++ src/main/java/me/iblitzkriegi/jdacommands/commands/Help.java
@@ -0,0 +1,11 @@
+package me.iblitzkriegi.jdacommands.commands;
+
+import me.iblitzkriegi.jdacommands.utilities.Command;
+import me.iblitzkriegi.jdacommands.utilities.wrappers.CommandEvent;
+
+public class Help extends Command {
+    @Override
+    public void execute(CommandEvent event, String[] args) {
+        
+    }
+}
