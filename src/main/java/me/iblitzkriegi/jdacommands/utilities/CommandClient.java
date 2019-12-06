106f90789370b7b3815de96326c147305e5f6091
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-05 23:27:42 -0500

    Convert CommandClient to listen for OnMessageReceieved instead

--- src/main/java/me/iblitzkriegi/jdacommands/utilities/CommandClient.java
+++ src/main/java/me/iblitzkriegi/jdacommands/utilities/CommandClient.java
@@ -1,7 +1,8 @@
 package me.iblitzkriegi.jdacommands.utilities;
 
 import me.iblitzkriegi.jdacommands.utilities.wrappers.BuiltCommand;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import me.iblitzkriegi.jdacommands.utilities.wrappers.CommandEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.Arrays;
@@ -9,11 +10,11 @@ import java.util.Arrays;
 public class CommandClient extends ListenerAdapter {
 
     @Override
-    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+    public void onMessageReceived(MessageReceivedEvent event) {
         String[] arguments = event.getMessage().getContentDisplay().split(" ");
         for (BuiltCommand builtCommand : CommandClientBuilder.commandHashMap.values()) {
             if (arguments[0].contains(CommandClientBuilder.commandStart + builtCommand.getName().toLowerCase())) {
-                builtCommand.getCommandClass().execute(event, Arrays.copyOfRange(arguments, 1, arguments.length));
+                builtCommand.getCommandClass().execute(new CommandEvent(event), Arrays.copyOfRange(arguments, 1, arguments.length));
             }
         }
     }
