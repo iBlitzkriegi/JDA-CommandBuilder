8884914639a781f03b9ac1feb2588d4996c25268
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-06 00:11:06 -0500

    Add RequiredPermissions interface

---
+++ src/main/java/me/iblitzkriegi/jdacommands/annotations/RequiredPermissions.java
@@ -0,0 +1,17 @@
+package me.iblitzkriegi.jdacommands.annotations;
+
+import net.dv8tion.jda.api.Permission;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface RequiredPermissions {
+    Permission[] value();
+}
+
+
