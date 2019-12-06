1a3a6da6599e0cf86b2e6fd2ec40175633a1f2a0
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-05 23:53:44 -0500

    Update CommandInfo.java

---
+++ src/main/java/me/iblitzkriegi/jdacommands/annotations/CommandInfo.java
@@ -0,0 +1,19 @@
+package me.iblitzkriegi.jdacommands.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CommandInfo {
+    String name();
+
+    String desc();
+
+    String usage();
+}
+
+
