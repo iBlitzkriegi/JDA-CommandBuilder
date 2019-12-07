2ca542b2f080329f595cfae4e06dfee29c07b493
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-07 14:07:13 -0500

    Add JDA parameter and make fields private

--- src/main/java/me/iblitzkriegi/jdacommands/utilities/wrappers/CommandClient.java
+++ src/main/java/me/iblitzkriegi/jdacommands/utilities/wrappers/CommandClient.java
@@ -1,15 +1,19 @@
 package me.iblitzkriegi.jdacommands.utilities.wrappers;
 
+import net.dv8tion.jda.api.JDA;
+
 import java.util.HashMap;
 
 public class CommandClient {
 
     public static HashMap<String, BuiltCommand> commandHashMap = new HashMap<>();
-    public static String commandStart = "";
+    private static String commandStart = "";
+    private static JDA jda;
 
-    public CommandClient(String prefix, HashMap<String, BuiltCommand> commands) {
+    public CommandClient(String prefix, HashMap<String, BuiltCommand> commands, JDA jda) {
         this.commandStart = prefix;
         this.commandHashMap = commands;
+        this.jda = jda;
     }
 
     public static BuiltCommand parseCommand(String string) {
