4fb585f41887361909d929952125a4da437c7d3c
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-06 23:15:26 -0500

    Add basic help command functionality

--- src/main/java/me/iblitzkriegi/jdacommands/commands/Help.java
+++ src/main/java/me/iblitzkriegi/jdacommands/commands/Help.java
@@ -2,7 +2,10 @@ package me.iblitzkriegi.jdacommands.commands;
 
 import me.iblitzkriegi.jdacommands.annotations.CommandInfo;
 import me.iblitzkriegi.jdacommands.utilities.Command;
+import me.iblitzkriegi.jdacommands.utilities.CommandClientBuilder;
+import me.iblitzkriegi.jdacommands.utilities.wrappers.BuiltCommand;
 import me.iblitzkriegi.jdacommands.utilities.wrappers.CommandEvent;
+import net.dv8tion.jda.api.MessageBuilder;
 
 @CommandInfo(
         name = "Help",
@@ -13,7 +16,15 @@ public class Help extends Command {
     @Override
     public void execute(CommandEvent event, String[] args) {
         if (args.length == 0) {
-            //Show default menu with all registered commands
+            MessageBuilder messageBuilder = new MessageBuilder();
+            messageBuilder.append("**" + event.getJDA().getSelfUser().getName() +"'s commands**\n");
+            for (BuiltCommand builtCommand : CommandClientBuilder.commandHashMap.values()) {
+                messageBuilder.append("`" + builtCommand.getName() + "` - " + builtCommand.getDescription() + "\n");
+            }
+            event.replyInDm(messageBuilder.build());
+            if (event.isFromGuild()) {
+                event.reply("My help menu has been sent to your DM's " + event.getAuthor().getAsMention() + "!");
+            }
             return;
         }
         //Take args[0] as a specified command and check CommandClientbuilder#commandHashMap for the command.
