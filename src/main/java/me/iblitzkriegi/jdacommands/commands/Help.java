8ef1ceace136c2d32653d560b19afeb395d853aa
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-06 23:26:17 -0500

    Finish up help command using CommandClientBuilder#parseCommand

--- src/main/java/me/iblitzkriegi/jdacommands/commands/Help.java
+++ src/main/java/me/iblitzkriegi/jdacommands/commands/Help.java
@@ -2,9 +2,11 @@ package me.iblitzkriegi.jdacommands.commands;
 
 import me.iblitzkriegi.jdacommands.annotations.CommandInfo;
 import me.iblitzkriegi.jdacommands.utilities.Command;
+import me.iblitzkriegi.jdacommands.utilities.CommandClient;
 import me.iblitzkriegi.jdacommands.utilities.CommandClientBuilder;
 import me.iblitzkriegi.jdacommands.utilities.wrappers.BuiltCommand;
 import me.iblitzkriegi.jdacommands.utilities.wrappers.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
 
 @CommandInfo(
@@ -30,6 +32,20 @@ public class Help extends Command {
             event.reply("You can only include a singular command to get information about.");
             return;
         }
-        //Take args[0] as a specified command and check CommandClientbuilder#commandHashMap for the command.
+        BuiltCommand builtCommand = CommandClientBuilder.parseCommand(args[0]);
+        if (builtCommand == null) {
+            event.reply("The provided input was not recognized as a valid command. Type `" + CommandClientBuilder.commandStart + "help` to see all the possible commands.");
+            return;
+        }
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Command Information(" + builtCommand.getName() + ")");
+        embedBuilder.addField("Command Name", builtCommand.getName().toLowerCase(), false);
+        if (!builtCommand.getDescription().isEmpty()) {
+            embedBuilder.addField("Command Description", builtCommand.getDescription(), false);
+        }
+        if (!builtCommand.getUsage().isEmpty()) {
+            embedBuilder.addField("Command Usage", builtCommand.getUsage(), false);
+        }
+        event.reply(embedBuilder.build());
     }
 }
