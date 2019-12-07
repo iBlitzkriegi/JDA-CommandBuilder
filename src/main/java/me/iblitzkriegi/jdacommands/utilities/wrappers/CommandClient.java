0013d0d34fe73b68676b962a8474fceebd4e10a5
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-07 14:00:25 -0500

    Create static hashMap and commandStart fields

--- src/main/java/me/iblitzkriegi/jdacommands/utilities/wrappers/CommandClient.java
+++ src/main/java/me/iblitzkriegi/jdacommands/utilities/wrappers/CommandClient.java
@@ -1,4 +1,12 @@
 package me.iblitzkriegi.jdacommands.utilities.wrappers;
 
+import java.util.HashMap;
+
 public class CommandClient {
+
+    public static HashMap<String, BuiltCommand> commandHashMap = new HashMap<>();
+    public static String commandStart = "";
+
+
+
 }
